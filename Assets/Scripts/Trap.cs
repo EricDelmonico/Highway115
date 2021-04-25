d8c0f4edeeca3ca1f3fc12357db42d5b0f43b598
vg1566 <vg1566@rit.edu>
2021-04-25 12:34:17 -0400

    Implemented traps

---
+++ Assets/Scripts/Trap.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Trap : MonoBehaviour
+{
+    public int power = 3;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Enemy")
+        {
+            collision.gameObject.GetComponent<Enemy>().Damage(power);
+            Destroy(gameObject);
+        }
+    }
+}
