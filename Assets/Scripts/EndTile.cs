720f56b3f99191bb41c817cd8acb8444ada27b1b
jf7689 <jf7689@rit.edu>
2021-05-03 15:43:19 -0400

    Transition animation working but not trigger, 2 new levels made

---
+++ Assets/Scripts/EndTile.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndTile : MonoBehaviour
+{
+    public Boss boss;
+    public bool levelEnded;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // doesn't seem to trigger, not sure is it because the boss is gone or due to gameObject not being active from start
+        if (boss.health <= 0)
+        {
+            gameObject.SetActive(true);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            levelEnded = true;
+        }
+    }
+}
